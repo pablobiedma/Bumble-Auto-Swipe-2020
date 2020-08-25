9f80358632a35b201e80a39347df9c43aa14ae9b
Pablo Biedma <43525988+pablobiedma@users.noreply.github.com>
2020-08-25 12:08:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-SwipeRight = setInterval(function () {
-    var Like = document.getElementsByClassName("button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) End(15px) Scale(1.1):h");
-    Like[0].click();
-}, 1000)
+x = setInterval(
+  function(){
+   var elem = document.getElementsByClassName("recsGamepad__button--like");
+   elem[0].click()
+  },200)
